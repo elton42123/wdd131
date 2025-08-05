70cd0e35a500c43f2e9030b3946288589ddb5cb9
elton42123 <ielton42123@gmail.com>
2025-08-05 11:51:28 +0300

    jk

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,5 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent =
   "Last Modification: " + document.lastModified;
+document.querySelector("#currentYear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = document.lastModified;  
